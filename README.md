3553e99d7407d5297708a030adffbb007ea4b180
sunxiaoning <xiaoning.sun@huolala.cn>
2024-08-17 15:07:31 +0800

    add checkhostip.sh

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # bashutils
 provide some useful bash utils
 
-## yum installer
+## 1. yum installer
 see `yuminstaller.sh`.
 
 ### function
@@ -13,7 +13,7 @@ use command `yuminstaller.sh -h` to find usage.
 ### about option
 use `-f` option to ignore old version already exists (allow version upgrade or override).
 
-## render
+## 2. file template render
 see `render.sh`.
 
 ### function
@@ -22,4 +22,15 @@ provide `file template render` .
 ### how to use
 TODO
 
+## 3. check host_ip
+see `checkhostip.sh`.
+
+### function
+check if the user-provided host_ip is valid.
+
+### how to use
+use command `checkhostip.sh -h` to find usage.
+
+### about option
+use the -l option to consider the loopback address as a valid host_ip.
 
---
+++ checkhostip.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+set -o pipefail
+
+USAGE="[-l] [-h] host_ip"
+LOOP_BACK=""
+LOOP_BACK_IP="127.0.0.1"
+
+function check-hostip() {
+  local host_ip="${1-}"
+  if [ -z "${host_ip}" ]; then
+    echo "host_ip param is invalid !" >&2
+    exit 1
+  fi
+
+  if [[ "${LOOP_BACK}" == "1" && ${host_ip} == "${LOOP_BACK_IP}" ]]; then
+    return 0
+  fi
+
+  local available_ips=$(hostname -I)
+
+  IFS=' ' read -r -a available_ips_array <<< "$available_ips"
+
+  for ip in "${available_ips_array[@]}"; do
+    if [ "$ip" == "${host_ip}" ]; then
+      return 0
+    fi
+  done
+
+  echo "host_ip: ${host_ip} is invalid!" >&2
+  exit 1
+}
+
+main() {
+  local opt_string=":lh"
+  local opt
+
+  #echo "Parsing arguments: $@ with opt_string: ${opt_string}"
+
+  while getopts "${opt_string}" opt; do
+    case ${opt} in
+    l)
+      LOOP_BACK="1"
+      ;;
+    h)
+      echo "Usage: ${0} ${USAGE}"
+      exit 0
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG, Usage: ${0} ${USAGE}" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+    esac
+  done
+  shift $((OPTIND - 1))
+
+  #echo "Remaining arguments after parsing: $@"
+
+  check-hostip "$@"
+}
+
+main "$@"
+
